9699441339ca40224ab084e26c74b7fcbb997e00
achintha <achinthathennakoon97@gmail.com>
2022-09-22 13:06:57 +0530

    added 3 cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
+import Details from "./components/Details/Details";
 
 function App() {
-  return <div>app</div>;
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
