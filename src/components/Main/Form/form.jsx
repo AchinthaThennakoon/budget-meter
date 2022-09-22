0893d715e5137b41fbce872ce30496e83e8db8a2
achintha <achinthathennakoon97@gmail.com>
2022-09-22 16:41:14 +0530

    structured elements

--- src/components/Main/Form/form.jsx
+++ src/components/Main/Form/form.jsx
@@ -1,7 +1,59 @@
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import useStyles from "./styles";
 
-const form = () => {
-  return <div>form</div>;
+const Form = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Grid>
+        <Grid item xs={12}>
+          <Typography align="center" variant="subtitle2" gutterBottom>
+            ...
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <InputLabel>Type</InputLabel>
+            <Select>
+              <MenuItem value="Income">Income</MenuItem>
+              <MenuItem value="Expense">Expense</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <InputLabel>category</InputLabel>
+            <Select>
+              <MenuItem value="business">business</MenuItem>
+              <MenuItem value="salary">salary</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField type="date" label="Date" fullWidth />
+          <Button
+            className={classes.button}
+            variant="outlined"
+            color="primary"
+            fullWidth
+          >
+            Create
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
-export default form;
+export default Form;
