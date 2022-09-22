2f1a50fd7a426fe5a8266c1ea56ffaa13bac1238
achintha <achinthathennakoon97@gmail.com>
2022-09-22 13:19:05 +0530

    added props to pass the title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import Details from "./components/Details/Details";
-import "./style";
+import useStyle from "./style";
 
 function App() {
+  const classes = useStyle();
   return (
     <>
       <Grid
+        className={classes.grid}
         container
         spacing={0}
         alignItems="center"
@@ -14,13 +16,13 @@ function App() {
         style={{ height: "100vh" }}
       >
         <Grid item xs={12} sm={4}>
-          <Details />
+          <Details title="Income" />
         </Grid>
         <Grid item xs={12} sm={4}>
           main
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Details />
+          <Details title="Expenses" />
         </Grid>
       </Grid>
     </>
