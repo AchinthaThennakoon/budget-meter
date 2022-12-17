a2aa62fd9cfbfb3df53a931a6e57bcbd148a08d7
achintha <achinthathennakoon97@gmail.com>
2022-12-17 16:39:13 +0530

    creates contextAPI

---
+++ src/context/context.js
@@ -0,0 +1,14 @@
+ import React, { createContext } from "react";
+
+
+ const initialState = [];
+ 
+ const ExpenseTrackerContext = createContext(initialState);
+
+ export const Provider = ({children})=>{
+    return(
+        <ExpenseTrackerContext.Provider value={{appName:'Expense Tracker'}}>
+            {children}
+        </ExpenseTrackerContext.Provider>
+    )
+ }
